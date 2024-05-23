6cfdab74e6a726c838c8305a2386565fe3cbc650
FrontMD <web@mockup.digital>
2024-05-23 11:55:25 +0300

    intros, socials, news-cards in progress

---
+++ src/blocks/modules/intro-slider-bg/intro-slider.js
@@ -0,0 +1,53 @@
+function introSliderBgInit() {
+    const introSection = document.querySelector("[data-js='introSectionBg']");
+    const introSlider = document.querySelector("[data-js='introSliderBg']");
+
+    if(!introSlider || !introSection) return
+
+    const sliderControls = introSection.querySelector("[data-js='sliderControls']"); 
+    const sliderPrev = sliderControls.querySelector("[data-js='sliderBtnPrev']");
+    const sliderNext = sliderControls.querySelector("[data-js='sliderBtnNext']");
+    const sliderPagination = sliderControls.querySelector("[data-js='sliderPagination']");
+
+    let introSliderEx = new Swiper(introSlider, {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        },
+        speed: 1000,
+        loop: true,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: sliderNext,
+            prevEl: sliderPrev,
+        },
+        pagination: {
+            el: sliderPagination,
+            type: 'fraction',
+        },
+        on: {
+            init: function(swiper){
+                if(swiper.slides.length < 2) {
+                    sliderControls.remove();
+                } else {
+                    sliderPagination.querySelectorAll('.swiper-pagination-current').forEach(item => {
+                        item.innerHTML = ('0' + (swiper.realIndex + 1)).slice(-2);
+                    });
+                    sliderPagination.querySelectorAll('.swiper-pagination-total').forEach(item => {
+                        item.innerHTML = ('0' + swiper.slides.length).slice(-2);
+                    });
+                }
+            },
+            slideChange: function(swiper) {
+                if(swiper.slides.length > 1) {
+                    sliderPagination.querySelectorAll('.swiper-pagination-current').forEach(item => {
+                        item.innerHTML = ('0' + (swiper.realIndex + 1)).slice(-2);
+                    });
+                    sliderPagination.querySelectorAll('.swiper-pagination-total').forEach(item => {
+                        item.innerHTML = ('0' + swiper.slides.length).slice(-2);
+                    });
+                }
+            },
+          }
+    })
+}
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,1 +1,8 @@
 @@include("../../blocks/modules/modals/modals.js")
+@@include("../../blocks/modules/intro-slider-bg/intro-slider.js")
+@@include("../../blocks/modules/intro-slider/intro-slider.js")
+
+document.addEventListener('DOMContentLoaded', () => {
+    introSliderInit();
+    introSliderBgInit();
+})
